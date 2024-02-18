812b3bf86ffefb1f337208e5fb89fb1c01b7fea7
RoelantD <roelant@icyt.nl>
2024-02-18 12:24:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='gym_mupen64plus',
                         'numpy==1.16.2',
                         'PyYAML==5.1',
                         'termcolor==1.1.0',
-                        'mss==4.0.2', # 4.0.3 removes support for Python 2.7
-                        'opencv-python==4.1.0.25'])
+                        'mss', # 4.0.3 removes support for Python 2.7
+                        'opencv-python'])
